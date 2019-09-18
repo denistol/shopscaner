80cd9b3a34b5040ef942b7c0d8ad1e93facddaf2
denistol <denistol@pm.me>
2019-09-18 16:04:20 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,69 @@
+if (!process.env.TELEGRAM_TOKEN) {
+    console.log('Telegram token not found');
+    process.exit(1);
+};
+
+const telegram = require('telegram-bot-api');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+const cheerio = require('cheerio');
+const api = new telegram({ token: process.env.TELEGRAM_TOKEN });
+const filePath = path.join(__dirname, 'data.json');
+const chatIds = ['266476630'];
+const url = `https://tehnoskarb.ua/igrovye-pristavki/c72?page=1?s=novelties`;
+
+const sendMessage = (text) => {
+    chatIds.forEach(chat_id => {
+        api.sendMessage({ chat_id, text })
+    });
+};
+
+const main = async () => {
+    const browser = await puppeteer.launch();
+    try {
+        const page = await browser.newPage();
+        await page.goto(url);
+        let content = await page.content().then(c => c);
+        let $ = cheerio.load(content, { decodeEntities: false });
+        let newResults = [];
+
+        // grab products
+        $('.c-item').each((index, el) => {
+            let item = $(el);
+            let url = item.find('a').attr('href');
+            if (!url) return;
+            let title = item.find('a').attr('title');
+            let count = /\d+/.exec(item.find('div.item-count').text());
+            let price = item.find('div.item-price').text();
+            newResults.push({ title, url, count: count ? count[0] : false, price });
+        });
+
+        if (!fs.existsSync(filePath)) {
+            fs.writeFileSync(filePath, JSON.stringify(newResults, null, 2));
+            return;
+        };
+        let oldResults = JSON.parse(fs.readFileSync(filePath));
+
+        newResults.forEach((val, index) => {
+            if (!oldResults.map(el => el['url']).includes(val['url'])) {
+                sendMessage(`Новый продукт https://tehnoskarb.ua/${val['url']}`);
+            };
+
+            oldResults.forEach((e, i) => {
+                if (e['url'] === val['url'] && Number(e['count']) < Number(val['count'])) {
+                    sendMessage(`Новое предложение https://tehnoskarb.ua/${val['url']}`);
+                }
+            })
+        });
+
+        fs.writeFileSync(filePath, JSON.stringify(newResults, null, 2));
+    }
+    catch {
+        console.log("[!] Bot error");
+        await browser.close();
+    };
+    await browser.close();
+};
+
+main();
