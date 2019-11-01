ca62bb16387404a36de66d12f85c360c7c334bf0
denistol <denistol@pm.me>
2019-11-01 13:35:00 +0200

    add new show

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const path = require('path');
 const cheerio = require('cheerio');
 const api = new telegram({ token: process.env.TELEGRAM_TOKEN });
 const filePath = path.join(__dirname, 'data.json');
+const filePath2 = path.join(__dirname, 'data2.json');
 const chatIds = ['371041145','266476630'];
 const url = `https://tehnoskarb.ua/igrovye-pristavki/c72?page=1?s=novelties`;
-
+const url2 = (page) => `https://technostock.com.ua/catalog/noutbuki-kompjutery-planshety/igrovye-konsoli?p=${page}`
 const sendMessage = (text) => {
     chatIds.forEach(chat_id => {
         api.sendMessage({ chat_id, text })
@@ -63,7 +64,33 @@ const main = async () => {
     catch {
         console.log("[!] Bot error");
         await browser.close();
-    };
+	};
+	// check tehnostock
+	const page = await browser.newPage();
+	await page.goto(url2(1));
+	let content = await page.content().then(c => c);
+	let $ = cheerio.load(content, { decodeEntities: false });
+	let newResults2 = {}
+	$('.item-inner').each( (i,el) => {
+		const item = $(el)
+		const url = item.find('a').attr('href')	
+		newResults2[i] = url
+	})
+	let oldResults2 = fs.existsSync(filePath2) ? JSON.parse(fs.readFileSync(filePath2)) : null
+	fs.writeFileSync(filePath2, JSON.stringify(newResults2, null, 2));
+
+	// diff
+	if(oldResults2){
+		const oldItems = Object.keys( oldResults2 ).map(el => oldResults2[el])
+		const newItems = Object.keys( newResults2 ).map(el => newResults2[el])
+		oldItems.forEach( el => {
+			if( !newItems.includes(el) ){
+				sendMessage(`Новое предложение ${el}`);
+			}
+		} )
+		console.log('done2')
+		
+	}
     await browser.close();
 };
 
